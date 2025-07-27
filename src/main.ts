7b22df4921f748eb140993561736b8cc68f6844d
Yash Sanghavi <yash.sanghavi@mordorintelligence.com>
2025-07-27 07:52:36 +0530

    feat: Update CORS configuration to include additional production domain for improved accessibility

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,8 @@ async function bootstrap() {
       origin: [
         "http://localhost:3000",
         "http://localhost:3001",
-        "https://stage.photoframix.com"
+        "https://stage.photoframix.com",
+        "https://photoframix.com"
       ],
       methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
       credentials: true,
